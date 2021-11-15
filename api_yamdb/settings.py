bda3105e4da77114911ce1c60ced1ce73bbe858e
art_nix <bobrolevv@yandex.ru>
2021-11-15 23:30:12 +0700

    fix remote tests 1.4

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(config['DEBUG'])
 
-ALLOWED_HOSTS = ['*']#config['ALLOWED_HOSTS']
+ALLOWED_HOSTS = ['*']  # config['ALLOWED_HOSTS']
 
 # Application definition
 INSTALLED_APPS = [
