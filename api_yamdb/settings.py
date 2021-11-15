58de0ffd7f198ccfd23f3d1ece6e61d7fe181d02
art_nix <bobrolevv@yandex.ru>
2021-11-15 23:33:49 +0700

    fix remote tests 1.5

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -13,10 +13,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config['SECRET_KEY']
+SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'  # config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(config['DEBUG'])
+DEBUG = False  # bool(config['DEBUG'])
 
 ALLOWED_HOSTS = ['*']  # config['ALLOWED_HOSTS']
 
