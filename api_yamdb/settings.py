ef70f55f6797f4a6af9b7b1aa2c279822589596b
art <bobrolevv@yandex.ru>
2021-11-16 16:14:19 +0700

    add .env

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-#####################
+######################
 # здесь пытался пройти тесты на платформе, поэтому много кода в комментах
 #####################
 
